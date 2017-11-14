5e814d56ca1310b395e7c2afed0f300da65ed068
Quentin <quentin.kuenlin@epfl.ch>
2017-11-14 14:29:22 +0100

    We can now click on node to highlight them

--- Viz/linkedGraph.js
+++ Viz/linkedGraph.js
@@ -12,21 +12,26 @@ var simulation = d3.forceSimulation()
 d3.json("movies.json", function (error, graph) {
     if (error) throw error;
 
+    var background = svg.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("fill", "white")
+        .on("click", function () { resetView(); });
+
     var link = svg.append("g")
-        //.attr("class", "links")
-        .selectAll("line")
+        .selectAll("link")
         .data(graph.links)
         .enter().append("line")
-        .attr("stroke", 0)
-        .attr("stroke-width", function (d) { return Math.sqrt(d.value); });
+        .attr("stroke", "#696969")
+        .attr("stroke-width", 1);
 
     var node = svg.append("g")
-        .attr("class", "node")
         .selectAll("circle")
         .data(graph.nodes)
         .enter().append("circle")
         .attr("r", 5)
         .attr("fill", function (d) { return color(d.group); })
+        .on("click", function (d) { click(d, this); });
 
     node.append("title")
         .text(function (d) { return d.id; });
@@ -45,6 +50,39 @@ d3.json("movies.json", function (error, graph) {
         node
             .attr("cx", function (d) { return d.x; })
             .attr("cy", function (d) { return d.y; });
+    }
+
+    function resetView() {
+        link.attr("stroke", "#696969")
+            .attr("stroke-width", 1)
+            .attr("stroke-opacity", 1);
+
+        node.attr("r", 5)
+            .attr("fill", function (d) { return color(d.group); })
+            .attr("opacity", 1);
+
+    }
+
+    function click(d, s) {
+        node.attr("opacity", function (n) {
+            if (d.id != n.id) return 0.3;
+        });
+
+
+        link.attr("stroke", function (x) {
+            if (x.source.id == d.id || x.target.id == d.id) {
+                node.attr("opacity", function (n) {
+                    if (x.source.id == n.id || x.target.id == n.id) return 1.0;
+                    else return "opacity";
+                });
+                return "red";
+            }
+            else return "#696969";
+        })
+        .attr("stroke-opacity", function (x) {
+            if (x.source.id == d.id || x.target.id == d.id) { return 1; }
+            else return 0.3;
+        })
     }
 
 });
